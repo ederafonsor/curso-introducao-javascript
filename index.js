9948657b84bb99a6ddfdd95a9d3f6b67668c06f0
Éder Afonso <ederafonsor@gmail.com>
2022-06-15 08:50:07 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import {Cliente} from "./Cliente.js"
+import {ContaCorrente} from "./ContaCorrente.js"
+
+const cliente1 = new Cliente("Ricardo", 11122233309);
+const cliente2 = new Cliente("Alice", 88822233309);
+
+const conta1 = new ContaCorrente(1001, cliente1);
+const conta2 = new ContaCorrente(102, cliente2);
+
+console.log(conta1);
+console.log(conta2);
+
+console.log('Número de contas abertas: ' + ContaCorrente.numeroDeContas);
+
